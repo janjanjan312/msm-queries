9136f635d774b5570d05c53b3a208a783a0f903c
janjanjan312 <1262325196@qq.com>
2024-11-06 02:48:14 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
 
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
 end
