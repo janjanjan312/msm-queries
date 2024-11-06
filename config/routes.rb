712818c8d6a64e8653ea1a97c37fdef3873a6a6e
janjanjan312 <1262325196@qq.com>
2024-11-06 02:14:10 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
 end
